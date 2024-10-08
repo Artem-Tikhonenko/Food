5cb36b4e723242b726225f280b59a839d614dac3
Artem Ti <Art.Tikhonenko@gmail.com>
2024-10-08 10:50:23 +0300

    past date processing

--- js/script.js
+++ js/script.js
@@ -45,11 +45,20 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const deadline = '2024-12-31';
 
     function getTimeRemainin(deadline) { //оределение временных промежутков
-        const t = Date.parse(deadline) - Date.parse(new Date()),
+        let days, hours,minutes,seconds;
+        const t = Date.parse(deadline) - Date.parse(new Date());
+
+        if(t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
               days = Math.floor(t / (1000 * 60 * 60 * 24)),
               hours = Math.floor((t / (1000 * 60 * 60) % 24)),
               minutes = Math.floor((t / (1000 * 60)) % 60),
               seconds = Math.floor((t / 1000) % 60);
+        }
 
         return {
             'total': t,
