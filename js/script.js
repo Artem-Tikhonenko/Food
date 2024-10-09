0b766cdce22dcdb5c5df8c7d4843c4a3adab1165
Artem Ti <Art.Tikhonenko@gmail.com>
2024-10-09 10:04:53 +0300

    modal close and open

--- js/script.js
+++ js/script.js
@@ -103,4 +103,38 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     setClock('.timer', deadline);
 
+    //Modal
+
+    const modalTriger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalClose = document.querySelector('[data-close]');
+
+    modalTriger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show'),
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide'),
+        modal.classList.remove('show');
+        document.body.style.overflow = ''; 
+    }
+
+    modalClose.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
 });
